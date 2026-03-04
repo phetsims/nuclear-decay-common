dcd056bc31dcba5cf7c1bab196b6845a2ccb5165
AgustinVallejo <agustin.vallejo@udea.edu.co>
2026-03-04 14:15:59 -0500

    Generating strings

---
+++ js/NuclearDecayCommonStrings.ts
@@ -0,0 +1,23 @@
+// Copyright 2026, University of Colorado Boulder
+
+/* eslint-disable */
+/* @formatter:off */
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/browser/getStringModule.js';
+import type LocalizedStringProperty from '../../chipper/js/browser/LocalizedStringProperty.js';
+import nuclearDecayCommon from './nuclearDecayCommon.js';
+
+type StringsType = {
+  'resetNucleus': string;
+  'resetNucleusStringProperty': LocalizedStringProperty;
+};
+
+const NuclearDecayCommonStrings = getStringModule( 'NUCLEAR_DECAY_COMMON' ) as StringsType;
+
+nuclearDecayCommon.register( 'NuclearDecayCommonStrings', NuclearDecayCommonStrings );
+
+export default NuclearDecayCommonStrings;
