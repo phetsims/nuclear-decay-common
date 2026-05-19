ef45fac60f5b6175e2af7c4c060c5050bad6b2a6
jbphet <jpblanco@yahoo.com>
2026-05-19 14:57:41 -0600

    sync-to-poly: add nucleus diameter, nucleon colors
    Mono-SHA: 65606ef9c361e15e1219cadc1b9ddd5bd0dc8296

--- js/common/view/DynamicNucleusNode.ts
+++ js/common/view/DynamicNucleusNode.ts
@@ -16,7 +16,7 @@ import optionize from '../../../../phet-core/js/optionize.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import Circle from '../../../../scenery/js/nodes/Circle.js';
 import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
-import Color from '../../../../scenery/js/util/Color.js';
+import ShredColors from '../../../../shred/js/ShredColors.js';
 import NuclearDecayAtom from '../model/NuclearDecayAtom.js';
 
 type SelfOptions = {
@@ -28,7 +28,10 @@ type DynamicNucleusNodeOptions = SelfOptions & NodeOptions;
 
 class DynamicNucleusNode extends Node {
 
-  private nucleonNodes: Node[] = [];
+  // the nodes used to represent the individual nucleons
+  private readonly nucleonNodes: Node[] = [];
+
+  private nucleusDiameter: number;
 
   public constructor(
     atom: NuclearDecayAtom,
@@ -47,36 +50,46 @@ class DynamicNucleusNode extends Node {
     let numberOfNucleonsToDisplay;
     // JPB REVIEW - this will need to be dynamically updated based on decay state.
     const totalNucleonCount = atom.atomConfigBeforeDecay.protonCount + atom.atomConfigAfterDecay.neutronCount;
-    if ( totalNucleonCount < 4 ) {
+    if ( totalNucleonCount < 5 ) {
 
       // Show them all for smaller numbers.
       numberOfNucleonsToDisplay = totalNucleonCount;
     }
     else {
 
-      // For larger numbers, show a subset. This value is determined in part by the equations for tightly packed
-      // particles in a spherical volume, but are empirically tweaked.
+      // For larger numbers, show a subset. This value is determined in part by the equation for tightly packed
+      // particles in a spherical volume, with the multiplier empirically tweaked.
       numberOfNucleonsToDisplay = Math.ceil( 4.8 * Math.pow( totalNucleonCount, 2 / 3 ) );
     }
 
     super( options );
 
+    // Calculate the diameter of the nucleus based on the number of nucleons.
+    this.nucleusDiameter = calculateNucleusDiameter( totalNucleonCount, 2 * options.nucleonRadius );
+
     const center = modelViewTransform.value.modelToViewPosition( atom.position );
 
     // Add the nucleon nodes.
-    _.times( numberOfNucleonsToDisplay, () => {
+    _.times( numberOfNucleonsToDisplay, count => {
+      const colorProperty = count < atom.atomConfigBeforeDecay.protonCount ?
+                            ShredColors.protonColorProperty :
+                            ShredColors.neutronColorProperty;
+
       const nucleusCircle = new Circle( options.nucleonRadius, {
-        fill: Color.GREEN.colorUtilsBrighter( 0.5 ),
-        stroke: Color.GREEN.colorUtilsDarker( 0.5 ),
+        fill: colorProperty,
         center: center.plus( this.getRandomNucleonOffsetVector() )
       } );
-      this.addChild( nucleusCircle );
       this.nucleonNodes.push( nucleusCircle );
     } );
+
+    const shuffledNucleonNodes = dotRandom.shuffle( this.nucleonNodes );
+    shuffledNucleonNodes.forEach( nucleonNode => this.addChild( nucleonNode ) );
 
     // Add a listener to the step timer that implements the dynamic motion of the particles in the nucleus.
     let timeAccumulator = 0;
     stepTimer.addListener( dt => {
+
+      // Only do the work for this if the nucleus is current visible and playing.
       if ( this.isVisible() && isPlayingProperty.value ) {
         timeAccumulator += dt;
         if ( timeAccumulator > 0.1 ) {
@@ -90,12 +103,18 @@ class DynamicNucleusNode extends Node {
     } );
   }
 
-  // JPB REVIEW: Static?  Or use a config option?
+  /**
+   * Get a random offset from the center of the nucleus for a nucleon.
+   */
   private getRandomNucleonOffsetVector(): Vector2 {
-    const length = ( dotRandom.nextDouble() - 0.5 ) * 20;
+    const length = ( dotRandom.nextDouble() - 0.5 ) * this.nucleusDiameter;
     return new Vector2( length, 0 ).rotated( dotRandom.nextDouble() * 2 * Math.PI );
   }
 
 }
+
+const calculateNucleusDiameter = ( numberOfNucleons: number, nucleonDiameter: number ): number => {
+  return nucleonDiameter * Math.pow( numberOfNucleons / 0.6, 1 / 3 );
+};
 
 export default DynamicNucleusNode;
