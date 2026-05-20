7d78e974c4fee6c085a2a7f326ba776adb73236a
jbphet <jpblanco@yahoo.com>
2026-05-19 18:11:35 -0600

    sync-to-poly: add initial set of alpha particles
    Mono-SHA: 155fb4c5cdf77e8be75f79db67ff4db970db7024

--- js/common/view/DynamicNucleusNode.ts
+++ js/common/view/DynamicNucleusNode.ts
@@ -12,12 +12,10 @@ import stepTimer from '../../../../axon/js/stepTimer.js';
 import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import dotRandom from '../../../../dot/js/dotRandom.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
+import affirm from '../../../../perennial-alias/js/browser-and-node/affirm.js';
 import optionize from '../../../../phet-core/js/optionize.js';
-import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
-import Circle from '../../../../scenery/js/nodes/Circle.js';
+import ShadedSphereNode from '../../../../scenery-phet/js/ShadedSphereNode.js';
 import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
-import Color from '../../../../scenery/js/util/Color.js';
-import RadialGradient from '../../../../scenery/js/util/RadialGradient.js';
 import ShredColors from '../../../../shred/js/ShredColors.js';
 import NuclearDecayAtom from '../model/NuclearDecayAtom.js';
 
@@ -30,15 +28,10 @@ type DynamicNucleusNodeOptions = SelfOptions & NodeOptions;
 
 class DynamicNucleusNode extends Node {
 
-  // the nodes used to represent the individual nucleons
-  private readonly nucleonNodes: Node[] = [];
-
-  private nucleusDiameter: number;
+  private readonly nucleusDiameter: number;
 
   public constructor(
     atom: NuclearDecayAtom,
-    // JPB REVIEW: Why is this a Property?
-    modelViewTransform: TReadOnlyProperty<ModelViewTransform2>,
     isPlayingProperty: TReadOnlyProperty<boolean>,
     providedOptions?: DynamicNucleusNodeOptions
   ) {
@@ -64,29 +57,34 @@ class DynamicNucleusNode extends Node {
       numberOfNucleonsToDisplay = Math.ceil( 4.8 * Math.pow( totalNucleonCount, 2 / 3 ) );
     }
 
+    // the nodes used to represent the individual nucleons
+    const nucleonNodes: ShadedSphereNode[] = [];
+
+    const alphaParticleNodes: Node[] = [];
+
     super( options );
 
     // Calculate the diameter of the nucleus based on the number of nucleons.
     this.nucleusDiameter = calculateNucleusDiameter( totalNucleonCount, 2 * options.nucleonRadius );
 
-    const center = modelViewTransform.value.modelToViewPosition( atom.position );
-
-    // Create the gradients used for proton and neutron fills.
-    const neutronFill = DynamicNucleusNode.createNucleonFill( ShredColors.neutronColorProperty, options.nucleonRadius );
-    const protonFill = DynamicNucleusNode.createNucleonFill( ShredColors.protonColorProperty, options.nucleonRadius );
-
     // Add the nucleon nodes.
     _.times( numberOfNucleonsToDisplay, count => {
-      const colorProperty = count < atom.atomConfigBeforeDecay.protonCount ? protonFill : neutronFill;
-
-      const nucleusCircle = new Circle( options.nucleonRadius, {
-        fill: colorProperty,
-        center: center.plus( this.getRandomNucleonOffsetVector() )
-      } );
-      this.nucleonNodes.push( nucleusCircle );
+      const nucleonNode = count < atom.atomConfigBeforeDecay.protonCount ?
+                          DynamicNucleusNode.createProtonNode( 2 * options.nucleonRadius ) :
+                          DynamicNucleusNode.createNeutronNode( 2 * options.nucleonRadius );
+      nucleonNodes.push( nucleonNode );
     } );
 
-    const shuffledNucleonNodes = dotRandom.shuffle( this.nucleonNodes );
+    _.times( 5, () => {
+      const alphaParticleNode = DynamicNucleusNode.createAlphaParticle(
+        2 * options.nucleonRadius,
+        dotRandom.nextDouble() * 2 * Math.PI
+      );
+      alphaParticleNodes.push( alphaParticleNode );
+      this.addChild( alphaParticleNode );
+    } );
+
+    const shuffledNucleonNodes = dotRandom.shuffle( nucleonNodes );
     shuffledNucleonNodes.forEach( nucleonNode => this.addChild( nucleonNode ) );
 
     // Add a listener to the step timer that implements the dynamic motion of the particles in the nucleus.
@@ -98,9 +96,11 @@ class DynamicNucleusNode extends Node {
         timeAccumulator += dt;
         if ( timeAccumulator > 0.1 ) {
           timeAccumulator = 0;
-          this.nucleonNodes.forEach( node => {
-            const offsetVector = this.getRandomNucleonOffsetVector();
-            node.center = center.plus( offsetVector );
+          nucleonNodes.forEach( node => {
+            node.center = this.getRandomNucleonOffsetVector();
+          } );
+          alphaParticleNodes.forEach( node => {
+            node.center = this.getRandomAlphaParticleOffsetVector();
           } );
         }
       }
@@ -111,24 +111,52 @@ class DynamicNucleusNode extends Node {
    * Get a random offset from the center of the nucleus for a nucleon.
    */
   private getRandomNucleonOffsetVector(): Vector2 {
+    const length = ( dotRandom.nextDouble() - 0.5 ) * this.nucleusDiameter * 0.75;
+    return new Vector2( length, 0 ).rotated( dotRandom.nextDouble() * 2 * Math.PI );
+  }
+
+  private getRandomAlphaParticleOffsetVector(): Vector2 {
     const length = ( dotRandom.nextDouble() - 0.5 ) * this.nucleusDiameter;
     return new Vector2( length, 0 ).rotated( dotRandom.nextDouble() * 2 * Math.PI );
   }
 
-  private static createNucleonFill(
-    baseColorProperty: TReadOnlyProperty<Color>,
-    nucleonRadius: number
-  ): RadialGradient {
-
-    return new RadialGradient(
-      -nucleonRadius * 0.4, -nucleonRadius * 0.4, 0,
-      -nucleonRadius * 0.4, -nucleonRadius * 0.4, nucleonRadius * 1.6 )
-      .addColorStop( 0, 'white' )
-      .addColorStop( 1, baseColorProperty.value );
+  private static createProtonNode( diameter: number ): ShadedSphereNode {
+    return new ShadedSphereNode( diameter, {
+      mainColor: ShredColors.protonColorProperty
+    } );
   }
 
+  private static createNeutronNode( diameter: number ): ShadedSphereNode {
+    return new ShadedSphereNode( diameter, {
+      mainColor: ShredColors.neutronColorProperty
+    } );
+  }
+
+  private static createAlphaParticle( nucleonDiameter: number, rotationalAngle: number ) : Node {
+    affirm( rotationalAngle >= 0 && rotationalAngle <= Math.PI * 2, 'out of range rotation angle' );
+    const p1 = DynamicNucleusNode.createProtonNode( nucleonDiameter );
+    const p2 = DynamicNucleusNode.createProtonNode( nucleonDiameter );
+    const n1 = DynamicNucleusNode.createNeutronNode( nucleonDiameter );
+    const n2 = DynamicNucleusNode.createNeutronNode( nucleonDiameter );
+    const nucleonPositioningVector = new Vector2( -nucleonDiameter / 2, 0 ).rotated( rotationalAngle );
+    n1.center = nucleonPositioningVector.copy();
+    nucleonPositioningVector.rotate( Math.PI );
+    n2.center = nucleonPositioningVector.copy();
+    nucleonPositioningVector.multiplyScalar( 0.75 ); // multiplier empirically determined
+    nucleonPositioningVector.rotate( Math.PI / 2 );
+    p1.center = nucleonPositioningVector.copy();
+    nucleonPositioningVector.rotate( Math.PI );
+    p2.center = nucleonPositioningVector.copy();
+
+    return new Node( { children: [ p2, n1, n2, p1 ] } );
+  }
 }
 
+/**
+ * Helper function to calculate the diameter of the nucleus given the number of nucleons and their diameter.  The
+ * calculation is based on sphere packing approximations, with the multiplier empirically tweaked to produce visually
+ * appealing results. Adjust as needed.
+ */
 const calculateNucleusDiameter = ( numberOfNucleons: number, nucleonDiameter: number ): number => {
   return nucleonDiameter * Math.pow( numberOfNucleons / 0.6, 1 / 3 );
 };
