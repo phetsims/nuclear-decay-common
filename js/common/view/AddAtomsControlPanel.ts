35352e1a15ef4f89d9bdea9e4a445fdd2fc53a79
AgustinVallejo <agustin.vallejo@udea.edu.co>
2026-05-26 14:22:38 -0500

    sync-to-poly: Using FineCoarseSpinner for the AddAtoms component
    Mono-SHA: b8f416450de4a0f2dea6f406a9921b001df17934

--- js/common/view/AddAtomsControlPanel.ts
+++ js/common/view/AddAtomsControlPanel.ts
@@ -8,15 +8,13 @@
  */
 
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
-import Property from '../../../../axon/js/Property.js';
 import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
-import optionize, { combineOptions } from '../../../../phet-core/js/optionize.js';
+import optionize from '../../../../phet-core/js/optionize.js';
 import WithRequired from '../../../../phet-core/js/types/WithRequired.js';
-import NumberDisplay from '../../../../scenery-phet/js/NumberDisplay.js';
+import FineCoarseSpinner from '../../../../scenery-phet/js/FineCoarseSpinner.js';
 import HBox from '../../../../scenery/js/layout/nodes/HBox.js';
 import VBox from '../../../../scenery/js/layout/nodes/VBox.js';
 import RichText from '../../../../scenery/js/nodes/RichText.js';
-import ArrowButton, { ArrowButtonOptions } from '../../../../sun/js/buttons/ArrowButton.js';
 import TextPushButton from '../../../../sun/js/buttons/TextPushButton.js';
 import NuclearDecayCommonColors from '../../NuclearDecayCommonColors.js';
 import NuclearDecayCommonConstants from '../../NuclearDecayCommonConstants.js';
@@ -41,8 +39,6 @@ export default class AddAtomsControlPanel extends NuclearDecayPanel {
       stepSize: 10
     }, providedOptions );
 
-    const maxAtoms = atomsToAddProperty.rangeProperty.value.max;
-
     const isotopeDynamicNameProperty = NuclearDecayModel.createDynamicIsotopeNameAndMassStringProperty(
       selectedIsotopeProperty,
       NuclearDecayCommonFluent.customStringProperty
@@ -53,64 +49,12 @@ export default class AddAtomsControlPanel extends NuclearDecayPanel {
       maxWidth: NuclearDecayCommonConstants.TEXT_MAX_WIDTH
     } );
 
-    // Arrow buttons for navigating isotopes
-    // Double arrows skip by element (proton count), single arrows step by isotope (neutron count)
-    const ARROW_BUTTON_OPTIONS = {
-      arrowHeight: 12,
-      arrowWidth: 12,
-      xMargin: 5,
-      yMargin: 5
-    };
-    const DOUBLE_ARROW_BUTTON_OPTIONS = {
-      arrowHeight: 12,
-      arrowWidth: 12,
-      xMargin: 5,
-      yMargin: 5,
-      numberOfArrows: 2,
-      arrowSpacing: -7
-    };
-    const stepSizeStringProperty = new Property( `${options.stepSize}` );
+    const numberOfAtomsSpinner = new FineCoarseSpinner( atomsToAddProperty, {
+      deltaFine: 1,
+      deltaCoarse: options.stepSize,
+      tandem: options.tandem.createTandem( 'numberOfAtomsSpinner' )
+    } );
 
-    const doubleLeftArrowButton = new ArrowButton( 'left', () => {
-      atomsToAddProperty.value = Math.max( 1, atomsToAddProperty.value - options.stepSize );
-    }, combineOptions<ArrowButtonOptions>( DOUBLE_ARROW_BUTTON_OPTIONS, {
-      accessibleName: NuclearDecayCommonFluent.a11y.multipleAtoms.numberOfAtomsControl.decreaseSampleByStepSize.createProperty( {
-        stepSize: stepSizeStringProperty
-      } ),
-      tandem: options.tandem.createTandem( 'doubleLeftArrowButton' ),
-      enabledProperty: atomsToAddProperty.derived( atoms => atoms > atomsToAddProperty.rangeProperty.value.min )
-    } ) );
-
-    const singleLeftArrowButton = new ArrowButton( 'left', () => {
-      atomsToAddProperty.value = Math.max( 1, atomsToAddProperty.value - 1 );
-    }, combineOptions<ArrowButtonOptions>( ARROW_BUTTON_OPTIONS, {
-      accessibleName: NuclearDecayCommonFluent.a11y.multipleAtoms.numberOfAtomsControl.decreaseSampleByOneStringProperty,
-      tandem: options.tandem.createTandem( 'singleLeftArrowButton' ),
-      enabledProperty: atomsToAddProperty.derived( atoms => atoms > atomsToAddProperty.rangeProperty.value.min )
-    } ) );
-
-    const singleRightArrowButton = new ArrowButton( 'right', () => {
-      atomsToAddProperty.value = Math.min( maxAtoms, atomsToAddProperty.value + 1 );
-    }, combineOptions<ArrowButtonOptions>( ARROW_BUTTON_OPTIONS, {
-      accessibleName: NuclearDecayCommonFluent.a11y.multipleAtoms.numberOfAtomsControl.increaseSampleByOneStringProperty,
-      tandem: options.tandem.createTandem( 'singleRightArrowButton' ),
-      enabledProperty: atomsToAddProperty.derived( atoms => atoms < atomsToAddProperty.rangeProperty.value.max )
-    } ) );
-
-    const doubleRightArrowButton = new ArrowButton( 'right', () => {
-      atomsToAddProperty.value = Math.min( maxAtoms, atomsToAddProperty.value + options.stepSize );
-    }, combineOptions<ArrowButtonOptions>( DOUBLE_ARROW_BUTTON_OPTIONS, {
-      accessibleName: NuclearDecayCommonFluent.a11y.multipleAtoms.numberOfAtomsControl.increaseSampleByStepSize.createProperty( {
-        stepSize: stepSizeStringProperty
-      } ),
-      tandem: options.tandem.createTandem( 'doubleRightArrowButton' ),
-      enabledProperty: atomsToAddProperty.derived( atoms => atoms < atomsToAddProperty.rangeProperty.value.max )
-    } ) );
-
-    // Number display showing atom count
-    const numberDisplay = new NumberDisplay( atomsToAddProperty, atomsToAddProperty.rangeProperty.value );
-
-    // Add button
     const addButton = new TextPushButton( NuclearDecayCommonFluent.addStringProperty, {
       font: NuclearDecayCommonConstants.CONTROL_FONT,
       baseColor: NuclearDecayCommonColors.addButtonProperty,
@@ -121,26 +65,12 @@ export default class AddAtomsControlPanel extends NuclearDecayPanel {
       tandem: options.tandem.createTandem( 'addButton' )
     } );
 
-    // Bottom row: arrows, display, and add button
     const controlsRow = new HBox( {
       spacing: 20,
       align: 'center',
-      children: [
-        new HBox( {
-          spacing: 4,
-          children: [
-            doubleLeftArrowButton,
-            singleLeftArrowButton,
-            numberDisplay,
-            singleRightArrowButton,
-            doubleRightArrowButton
-          ]
-        } ),
-        addButton
-      ]
+      children: [ numberOfAtomsSpinner, addButton ]
     } );
 
-    // Main layout: title on top, controls below
     const contentNode = new VBox( {
       spacing: 8,
       xMargin: 20,
