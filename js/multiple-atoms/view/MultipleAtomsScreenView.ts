762892c98052d54d48bc367a25759b777c6ad091
AgustinVallejo <agustin.vallejo@udea.edu.co>
2026-06-04 15:45:02 -0500

    sync-to-poly: Proper stopwatch behavior: autoplay when T=0 for better UX but don't if time has already passed
    Mono-SHA: 895d8e996ed395de1f1f56e662e9152be2c3cb2f

--- js/multiple-atoms/view/MultipleAtomsScreenView.ts
+++ js/multiple-atoms/view/MultipleAtomsScreenView.ts
@@ -218,7 +218,7 @@ export default class MultipleAtomsScreenView extends SingleAndMultipleAtomsScree
     // Making stopwatch run when activated, for better user experience reasons.
     // This is fine because sim won't actually run until atoms are added.
     stopwatchNode.visibleProperty.link( visible => {
-      stopwatch.isRunningProperty.value = visible;
+      stopwatch.isRunningProperty.value = visible && model.timeProperty.value === 0;
     } );
 
     this.children = [ this.playAreaBoundsRectangle, ...this.children ];
