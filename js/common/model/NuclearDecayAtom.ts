744dd26305ac12b8b4e03486da07e20f065d3eac
AgustinVallejo <agustin.vallejo@udea.edu.co>
2026-06-08 16:03:40 -0500

    sync-to-poly: Disabling quantum tunneling via the preference
    Mono-SHA: cb3d7821839e91460c793a249b51b53b2049af04

--- js/common/model/NuclearDecayAtom.ts
+++ js/common/model/NuclearDecayAtom.ts
@@ -84,10 +84,6 @@ type SelfOptions = {
 
   // The speed at which ejected decay particles move, in model units per second.
   particleEjectionSpeed?: number;
-
-  // When this value is true, the model may start the ejection decay products at a distance outside the nucleus.  When
-  // false, ejected particles will come from the center of the nucleus.
-  showQuantumTunneling?: boolean;
 };
 
 export type NuclearDecayAtomOptions = SelfOptions;
@@ -153,8 +149,9 @@ export default class NuclearDecayAtom {
   // The type of decay that this nucleus will undergo.
   private decayType: DecayType;
 
-  // See option type info for explanation.
-  private showQuantumTunneling: boolean;
+  // When this value is true, the model may start the ejection decay products at a distance outside the nucleus.  When
+  // false, ejected particles will come from the center of the nucleus.
+  public showQuantumTunneling = true;
 
   // JB REVIEW: Discuss with AV.  Is this a reasonable way to do this?
   // For atoms that decay in a way the uses quantum tunneling, such as alpha decay, this is the radius at which the
@@ -170,8 +167,7 @@ export default class NuclearDecayAtom {
     const options = optionize<NuclearDecayAtomOptions, SelfOptions, NuclearDecayAtomOptions>()( {
       ejectParticlesOnDecay: true,
       restrictEjectionAngles: false,
-      particleEjectionSpeed: DEFAULT_PARTICLE_EJECTION_SPEED,
-      showQuantumTunneling: true
+      particleEjectionSpeed: DEFAULT_PARTICLE_EJECTION_SPEED
     }, providedOptions );
 
     this.isotope = isotope;
@@ -183,7 +179,6 @@ export default class NuclearDecayAtom {
     this.restrictEjectionAngles = options.restrictEjectionAngles;
     this.particleEjectionSpeed = options.particleEjectionSpeed;
     this.decayType = decayType;
-    this.showQuantumTunneling = options.showQuantumTunneling;
 
     const halfLife = AtomInfoUtils.getNuclideHalfLife(
       atomConfigBeforeDecay.protonCount,
@@ -341,7 +336,6 @@ export default class NuclearDecayAtom {
       'halfLife can only be set directly on custom atoms' );
     this._halfLife = value;
   }
-
 
   /**
    * Get the atom config of an arbitrary isotope
